062e17f422e9d1aa91d9f4d6a50584291d0f2d9b
Radi <radi03825@gmail.com>
2022-10-13 18:49:12 +0300

    Commit

---
+++ Introduction to programming/sumOfTheDigits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    int number;
+
+    cout << "Enter 3-digit number: ";
+    cin >> number;
+
+    int firstDigit = number / 100;
+    int secondDigit = (number / 10) % 10;
+    int thirdDigit = number % 10;
+
+    int sum = firstDigit + secondDigit + thirdDigit;
+
+    cout << "First digit: " << firstDigit << endl;
+    cout << "Second digit: " << secondDigit << endl;
+    cout << "Third digit: " << thirdDigit << endl;
+
+    cout << "Sum of the digits: " << sum;
+}
