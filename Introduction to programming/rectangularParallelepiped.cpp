2d68f3cb1dc0825a15e4d8dfc489172551c66818
Radi <radi03825@gmail.com>
2022-10-13 18:44:02 +0300

    first commit

---
+++ Introduction to programming/rectangularParallelepiped.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    int a, b, c;
+
+    cout << "a = ";
+    cin >> a;
+
+    cout << "b = ";
+    cin >> b;
+
+    cout << "c = ";
+    cin >> c;
+
+    int area = 2 * (a * b + a * c + b * c);
+    int volume = a * b * c;
+
+    cout << "Surface area = " << area << endl;
+    cout << "Volume = " << volume;
+}
