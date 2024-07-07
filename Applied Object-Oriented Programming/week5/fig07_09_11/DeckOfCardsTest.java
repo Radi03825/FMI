1003a8ea33eb2e1365ad5c3b60b3c047c5ea737f
Radoslav Angelov <106675651+Radi03825@users.noreply.github.com>
2024-07-07 17:33:17 +0300

    Commit

---
+++ Applied Object-Oriented Programming/week5/fig07_09_11/DeckOfCardsTest.java
@@ -0,0 +1,48 @@
+// Fig. 7.11: DeckOfCardsTest.java
+// Card shuffling and dealing application.
+
+public class DeckOfCardsTest
+{
+   // execute application
+   public static void main( String args[] )
+   {
+      DeckOfCards myDeckOfCards = new DeckOfCards();
+      myDeckOfCards.shuffle(); // place Cards in random order
+
+      System.out.printf("%-20s%-20s\n", "Left hand:", "Right hand:");
+
+      Card[] leftHand = myDeckOfCards.dealHand();
+      Card[] rightHand = myDeckOfCards.dealHand();
+
+      for (int i = 0; i < leftHand.length; i++) {
+         System.out.printf("%-20s%-20s\n", leftHand[i].toString(), rightHand[i].toString());
+      }
+
+      System.out.println("\nHand Values:");
+
+      int leftHandRank = myDeckOfCards.getHandRank(leftHand);
+      int rightHandRank = myDeckOfCards.getHandRank(rightHand);
+
+      System.out.printf("%-20s%-20s\n", myDeckOfCards.getRankLabel(leftHandRank), myDeckOfCards.getRankLabel(rightHandRank));
+
+      if (leftHandRank < rightHandRank) {
+         System.out.println("\nResult: left hand is better.");
+      } else if (leftHandRank > rightHandRank) {
+         System.out.println("\nResult: right hand is better.");
+      } else {
+         System.out.println("\nResult: both hands is equal.");
+      }
+
+      // print all 52 Cards in the order in which they are dealt
+//      for ( int i = 0; i < 13; i++ )
+//      {
+//         // deal and print 4 Cards
+//         System.out.printf( "%-20s%-20s%-20s%-20s\n",
+//            myDeckOfCards.dealCard(), myDeckOfCards.dealCard(),
+//            myDeckOfCards.dealCard(), myDeckOfCards.dealCard() );
+//      } // end for
+   } // end main
+
+
+} // end class DeckOfCardsTest
+
