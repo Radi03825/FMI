f55c90a8afddd1350c2c02c288af7b920a228def
Radoslav Angelov <106675651+Radi03825@users.noreply.github.com>
2024-07-14 16:19:02 +0300

    Remove some files

--- Applied Object-Oriented Programming/week12/lab12/src/Planet.java
+++
@@ -1,6 +0,0 @@
-//public abstract sealed class Planet permits Mercury, Venus, Earth {}
-//
-//non-sealed class Venus {}
-//non-sealed class Mars extends Planet {}
-//non-sealed class Mercury {}
-//abstract sealed class Earth {}
--- Applied Object-Oriented Programming/week12/lab12/src/b.java
+++
@@ -1,2 +0,0 @@
-public interface b extends a{
-}
