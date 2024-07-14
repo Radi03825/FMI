4b3d61e7170387cdcd2f791e98d5e6236f0f18ec
Radoslav Angelov <106675651+Radi03825@users.noreply.github.com>
2024-07-14 15:49:18 +0300

    Commit week 10

---
+++ Applied Object-Oriented Programming/week10/problem1/test/src/test/TabulateTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import functions.Tabulate;
+import services.Computable;
+
+public class TabulateTest {
+    public static void main(String[] args) {
+        Tabulate tab = new Tabulate(null);
+
+        tab.tabulate(1, 101, 25);
+
+        Tabulate.tabulateFunction(1, 10, 5, new Computable() {
+            @Override
+            public double function(double x) {
+                if (x == 0) {
+                    return 0;
+                }
+
+                return 1 / x;
+            }
+        });
+    }
+}
